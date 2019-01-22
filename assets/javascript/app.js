617a0b0e84b6a0de54677531902da33bd511efbb
Tim Welsh <tim@timwelsh.com>
2019-01-22 18:26:23 -0500

    making progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,50 @@
 //JS for Giftastic
 
 //Original list of GIFs
-var originalGifList = ['spider', 'snake','bats', 'the shining'];
+// var originalGifList = ['spider', 'snake','bats', 'the shining'];
+// var inputText = "";
 
-for(var i = 0; i < originalGifList.length; i++) {
-    buttonGif = $("<button>").text(originalGifList[i]).addClass("btn btn-dark mr-2 char")
-    $(".gifButtons").append(buttonGif);
-}
+// for(var i = 0; i < originalGifList.length; i++) {
+//     buttonGif = $("<button>").text(originalGifList[i]).addClass("btn btn-dark mr-2 data-creep")
+//     $(".gifButtons").append(buttonGif);
+// }
 
 $("form").submit(function(event) {
     event.preventDefault();
     var inputText = $("#inputGif").val();
-    newButton = $("<button>").text(inputText).addClass("btn btn-dark char");
-    $("#gif-list").append(newButton);
-    console.log(inputText + " here ");
+    newButton = $("<button>").text(inputText).addClass("btn btn-dark data-creep");
+    $(".gifButtons").append(newButton);
+    // console.log(inputText + " here ");
 });
 
-$(document).on("click", ".char", function() {
+$(document).on("click", ".data-creep", function() {
+    var creep = $(this).attr("data-creep");
     character = $(this).text();
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=Ano70sddTNMYHNQVNpgeQdCCClqQr93n";
-    console.log(character + " char ");
-
+    var apiKey = "api_key=Ano70sddTNMYHNQVNpgeQdCCClqQr93n"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?" + apiKey + "&q=" + creep + "&limit=20&offset=0&rating=G&lang=en"
+    console.log(' api ' + queryURL);
     $.ajax({
     url: queryURL,
     method: "GET"
     })
     .then(function(response) {
-        for (var i = 0; i < 25; i++) {
-            console.log(response.data[i].url);
-            // HTMLContent = '<div><img src="' + data.image + ' title="' + data.subTitle + ' alt="Photo"></div>';
-            // I do not want to append the DOM here
-            // return HTMLContent doesn't work
+        var results = response.data;
+        console.log(response);
+        var div = $(".gifButtons");
+        const img = $("<img>");
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var gifImage = $("<img>");
+            // console.log(results[i].images.fixed_width_still.url);
+            gifImage.attr("src", results[i].images.fixed_width_still.url);
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+            $("#gifs-here").prepend(gifDiv);
+            // img.attr('src', response.data[0].images.fixed_width_still.url);
+            // div.append(img);
+            // console.log(response.data[i].url);
         }
-    
     });
 })
