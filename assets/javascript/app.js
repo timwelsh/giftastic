732158eced849e0e2177f22cbce2872f6af46321
Tim Welsh <tim@timwelsh.com>
2019-01-23 15:28:37 -0500

    more changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,66 @@
 //JS for Giftastic
+$(document).ready(function() {
 
-//Original list of GIFs
 
-// for(var i = 0; i < originalGifList.length; i++) {
-//     buttonGif = $("<button>").text(originalGifList[i]).addClass("btn btn-dark mr-2 data-creep")
-//     $(".gifButtons").append(buttonGif);
-// }
+    //Original list of GIFs
+    var originalGifList = ["spider", "bat", "the shining", "centipeded"]
 
-$("form").submit(function(event) {
-    event.preventDefault();
-    var inputText = $("#inputGif").val();
-    newButton = $("<button>").text(inputText).addClass("btn btn-dark data-creep");
-    $(".gifButtons").append(newButton);
-});
 
-$("button").on("click", function() {
-    var creep = $(this).attr("data-creep");
-    console.log(creep + " creeper ");
-    var apiKey = "api_key=Ano70sddTNMYHNQVNpgeQdCCClqQr93n"
-    var queryURL = "https://api.giphy.com/v1/gifs/search?" + apiKey + "&q=" + creep + "&limit=20&offset=0&rating=G&lang=en"
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-    })
-    .then(function(response) {
-        var results = response.data;
-
-        var newDiv = $(".gifButtons");
-        const img = $("<img>");
-        for (var i = 0; i < results.length; i++) {
-            console.log(results[i].images.fixed_width_still.url);
-            var newDiv = $("<div>");
-            // var rating = results[i].rating;
-            // var p = $("<p>").text("Rating: " + rating);
-            var newImage = $("<img>");
-            // console.log(results[i].images.fixed_width_still.url);
-            newImage.attr("src", results[i].images.fixed_width_still.url);
-            // newDiv.append(p);
-            newDiv.append(newImage);
-            $("#gifs-here").prepend(newDiv);
-
-            // img.attr('src', response.data[0].images.fixed_width_still.url);
-            // div.append(img);
-            // console.log(response.data[i].url);
-        }
+    $("form").submit(function(event) {
+        event.preventDefault();
+        var inputText = $("#inputGif").val();
+        newButton = $("<button>").text(inputText).addClass("btn btn-dark data-creep");
+        $(".gifButtons").append(newButton);
     });
+
+    function buildButtons() {
+        $(".gifButtons").empty()
+        for(var i = 0; i < originalGifList.length; i++) {
+            var buttonGif = $("<button>").text(originalGifList[i])
+            buttonGif.addClass("btn btn-dark mr-2 data-creep")
+            $(".gifButtons").append(buttonGif);
+        }
+    }
+    buildButtons();
+
+    $(document).on("click", ".creep", function() {
+        var creep = $(this).attr("data-creep");
+        var apiKey = "api_key=Ano70sddTNMYHNQVNpgeQdCCClqQr93n"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?" + apiKey + "&q=" + creep + "&limit=10&offset=0&rating=G&lang=en"
+        
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+        .then(function(response) {
+            var result = $(this).attr("data-creep")
+            // var newDiv = $(".gifButtons");
+            // const img = $("<img>");
+
+
+            for (var i = 0; i < result.length; i++) {
+                console.log(result);
+                var stillImage = response.data[i].images.fixed_width_still.url;
+                var animateImage = response.data[i].images.fixed_width.url;
+                var newImage = $("<img>");
+                var gifDiv = $("<div class='gif clearfix' style='float:left'>");
+                // var rating = results[i].rating;
+                // var p = $("<p>").text("Rating: " + rating);
+                // console.log(results[i].images.fixed_width_still.url);
+
+                newImage.attr("src", newImage);
+                newImage.attr("data-still", stillImage);
+                newImage.attr("data-animate", animateImage);
+
+                // newDiv.append(p);
+                gifDiv.append(newImage);
+                // newDiv.append(newImage);
+                $("#gifs-here").prepend(gifDiv);
+
+                // img.attr('src', response.data[0].images.fixed_width_still.url);
+                // div.append(img);
+                // console.log(response.data[i].url);
+            }
+        });
+    })
 })
