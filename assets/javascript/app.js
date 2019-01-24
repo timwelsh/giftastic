c264da185717a2d91c4e0292b3085bb250766371
Tim Welsh <tim@timwelsh.com>
2019-01-24 09:38:20 -0500

    displaying something

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,14 +5,16 @@ $(document).ready(function() {
     //Original list of GIFs
     var originalGifList = ["spider", "bat", "the shining", "centipeded"]
 
-
+    //create new button - working!
     $("form").submit(function(event) {
         event.preventDefault();
         var inputText = $("#inputGif").val();
-        newButton = $("<button>").text(inputText).addClass("btn btn-dark data-creep");
+        newButton = $("<button>").text(inputText).addClass("btn btn-dark");
+        newButton.attr("data-creep", inputText);
         $(".gifButtons").append(newButton);
     });
 
+    // build buttons to display from the array above - working!
     function buildButtons() {
         $(".gifButtons").empty()
         for(var i = 0; i < originalGifList.length; i++) {
@@ -26,41 +28,30 @@ $(document).ready(function() {
     $(document).on("click", function() {
         var creep = $(this).attr("data-creep");
         var apiKey = "api_key=Ano70sddTNMYHNQVNpgeQdCCClqQr93n"
-        var queryURL = "https://api.giphy.com/v1/gifs/search?" + apiKey + "&q=" + creep + "&limit=10&offset=0&rating=G&lang=en"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?" + apiKey + "&q=" + creep + "&limit=10&rating=G"
         
         $.ajax({
         url: queryURL,
         method: "GET"
         })
         .then(function(response) {
-            // var result = $(this).attr("data-creep")
-            // var newDiv = $(".gifButtons");
-            // const img = $("<img>");
+            console.log(queryURL);
 
-
-            for (var i = 0; i < response.data.length; i++) {
-                console.log(response);
-                
-                var stillImage = response.data[i].images.fixed_width_still.url;
-                var animateImage = response.data[i].images.fixed_width.url;
+            for (var i = 0; i < 10; i++) {
+                var newDiv = $("<div class='newDiv'>");
+                var p = $("<p>").text("Rating: " + response.data[i].rating);
                 var newImage = $("<img>");
-                var gifDiv = $("<div class='gif clearfix' style='float:left'>");
-                // var rating = results[i].rating;
-                // var p = $("<p>").text("Rating: " + rating);
-                // console.log(results[i].images.fixed_width_still.url);
+                p.attr("class", "gif")
+                newImage.attr("src", response.data[i].images.downsized_still.url);
+				newImage.attr("data-still", response.data[i].images.fixed_width_still.url);
+                newImage.attr("data-animate", response.data[i].images.original.url);
+				newImage.attr("data-state", "still"); 
+				newImage.attr("class", "gif");
+                // prepend in new div
+                newDiv.prepend(p);
+                newDiv.prepend(newImage);
+                $(".gifs-here").prepend(newDiv);
 
-                newImage.attr("src", newImage);
-                newImage.attr("data-still", stillImage);
-                newImage.attr("data-animate", animateImage);
-
-                // newDiv.append(p);
-                gifDiv.append(newImage);
-                // newDiv.append(newImage);
-                $("#gifs-here").prepend(gifDiv);
-
-                // img.attr('src', response.data[0].images.fixed_width_still.url);
-                // div.append(img);
-                // console.log(response.data[i].url);
             }
         });
     })
